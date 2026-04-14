eb96ca111bfdf0c7318b8727b8ed279ca6875c82
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2026-04-14 14:03:47 +0530

    Added System to Render Camera and Sprite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::{prelude::*, sprite};
+
 fn main() {
-    println!("Hello, world!");
+    //Main Loader for bevy
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, SpawnCamera)
+        .run();
+}
+
+fn SpawnCamera(mut commands: Commands) {
+    // This will spawn camera at 1000 z axis
+    commands.spawn((
+        Camera2d,
+        Transform::from_xyz(0.0, 0.0, 1000.0),
+        GlobalTransform::default(),
+    ));
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::hsl(0.2, 0.3, 0.7),
+            custom_size: Some(Vec2::new(120.0, 80.0)),
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    });
 }
